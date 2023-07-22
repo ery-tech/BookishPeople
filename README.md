f5efe2b93b85a940ff9fa2b1417875665e1125f9
Erika <erikaguardo99@gmail.com>
2023-07-22 15:47:56 +0200

    readMe

--- README.md
+++ README.md
@@ -3,7 +3,10 @@
 <img src="https://github.com/ery-tech/BookishPeople/assets/117271118/22f0a693-2a7c-485e-9646-c01a553e2848" width="800px" height="400px" style="position:absolute" frameBorder="0" class="giphy-embed" alt="screenshot"/>
 </div>
 <h2> Description </h2>
-
+The aim of this application is to encourage people to read more books.
+The application  consists of a simple textbox  to allow the user to search for all the books in a specific category.
+When the user clicks on a special button, the application will contact the API of the Open Library  service. Once the list of books has been retrieved, the application will only have to display the title and the list of authors.
+When the user clicks on a special button, the application will display the description of the book.
 
 
 
@@ -28,5 +31,6 @@
 
    <h4>Deploy link:     </h4>
   <span> 
-  <img align="left" src="https://img.shields.io/badge/netlify-%23000000.svg?style=for-the-badge&logo=netlify&logoColor=#00C7B7  "> 
+  <img align="left" src="https://img.shields.io/badge/netlify-%23000000.svg?style=for-the-badge&logo=netlify&logoColor=#00C7B7  ">
+   https://bookish-people.netlify.app/
 </span>
--- src/app/interceptors/loading.interceptor.spec.ts
+++ src/app/interceptors/loading.interceptor.spec.ts
@@ -1,10 +1,10 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, fakeAsync } from '@angular/core/testing';
 
 import { LoadingInterceptor } from './loading.interceptor';
 import { LoaderService } from '../services/loader.service';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { HTTP_INTERCEPTORS, HttpHandler, HttpRequest } from '@angular/common/http';
-import { finalize } from 'rxjs';
+import { finalize, take } from 'rxjs';
 
 describe('LoadingInterceptor', () => {
   let loaderService: LoaderService
@@ -47,5 +47,20 @@ describe('LoadingInterceptor', () => {
     expect(loaderService.getLoading()).toBeTruthy();
     });
 
+    it('should decrease total requests and activate loader', fakeAsync(() =>{
+      const request = new HttpRequest('GET', 'https://test.com/api');
 
-});
+       interceptor.intercept(request, httpHandle).pipe(
+      finalize( ()=>   {
+        expect(loaderService.setLoading(true)).toHaveBeenCalledTimes(1);
+        expect(loaderService.setLoading(false)).toHaveBeenCalledTimes(1);
+        expect(interceptor.totalRequests).toBe(0)
+        expect(loaderService.getLoading()).toBeFalse()
+
+
+
+      })
+      )}));
+
+    });
+
